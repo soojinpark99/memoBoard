4f49c6c44ec4a33ff0108bb77d5b649c8f9829b5
cree-per-com <kcossld-naver.com>
2024-02-17 13:14:04 +0900

    import 부분 정리

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -1,28 +1,15 @@
 package com.study.board.service;
 
-import com.study.board.BoardApplication;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
-import jakarta.servlet.http.HttpServletResponse;
-import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Base64;
 import java.util.UUID;
 
 @Service
